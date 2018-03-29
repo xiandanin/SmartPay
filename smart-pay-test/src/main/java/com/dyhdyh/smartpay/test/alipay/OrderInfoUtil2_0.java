977618a9524b1de04abcbaec952bb437d745d64f
dengyuhan <464958567@qq.com>
2018-03-29 17:25:12 +0800

    调整注释

--- smart-pay-test/src/main/java/com/dyhdyh/smartpay/test/alipay/OrderInfoUtil2_0.java
+++ smart-pay-test/src/main/java/com/dyhdyh/smartpay/test/alipay/OrderInfoUtil2_0.java
@@ -15,55 +15,11 @@ import java.util.Random;
 public class OrderInfoUtil2_0 {
 
     /**
-     * 构造授权参数列表
-     *
-     * @param pid
-     * @param app_id
-     * @param target_id
-     * @return
-     */
-    public static Map<String, String> buildAuthInfoMap(String pid, String app_id, String target_id, boolean rsa2) {
-        Map<String, String> keyValues = new HashMap<String, String>();
-
-        // 商户签约拿到的app_id，如：2013081700024223
-        keyValues.put("app_id", app_id);
-
-        // 商户签约拿到的pid，如：2088102123816631
-        keyValues.put("pid", pid);
-
-        // 服务接口名称， 固定值
-        keyValues.put("apiname", "com.alipay.account.auth");
-
-        // 商户类型标识， 固定值
-        keyValues.put("app_name", "mc");
-
-        // 业务类型， 固定值
-        keyValues.put("biz_type", "openservice");
-
-        // 产品码， 固定值
-        keyValues.put("product_id", "APP_FAST_LOGIN");
-
-        // 授权范围， 固定值
-        keyValues.put("scope", "kuaijie");
-
-        // 商户唯一标识，如：kkkkk091125
-        keyValues.put("target_id", target_id);
-
-        // 授权类型， 固定值
-        keyValues.put("auth_type", "AUTHACCOUNT");
-
-        // 签名类型
-        keyValues.put("sign_type", rsa2 ? "RSA2" : "RSA");
-
-        return keyValues;
-    }
-
-    /**
      * 构造支付订单参数列表
-     *
-     * @param pid
-     * @param app_id
-     * @param target_id
+     * @param app_id appid
+     * @param title 标题
+     * @param desc 描述
+     * @param rsa2 密钥
      * @return
      */
     public static Map<String, String> buildOrderParamMap(String app_id, String title, String desc, boolean rsa2) {
